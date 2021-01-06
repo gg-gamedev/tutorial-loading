a584564930d339aae573d69801a69aeb91f690a2
gg-gamedev <77074909+gg-gamedev@users.noreply.github.com>
2021-01-06 17:07:03 -0300

    - Tutorial content

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject m_PauseScreen;
+    public bool m_isPaused = false;
+
+    private void Awake()
+    {
+        //verifica se o loading ja foi instanciado
+        if (GameObject.Find("=LOADING=") == null)
+        {
+            GameObject loading = Instantiate(Resources.Load<GameObject>("Prefabs/Loading"));
+            loading.name = "=LOADING=";
+        }
+    }
+
+    void Update()
+    {
+        PauseGame();
+    }
+
+    /// <summary>
+    /// Pausa o jogo se botão "cancel" for pressionado
+    /// </summary>
+    private void PauseGame ()
+    {
+        //Se botão com nome "Cancel" for pressionado
+        if (Input.GetButtonDown("Cancel"))
+        {
+            //verifica se o jogo está pausado
+            if (!m_isPaused)
+            {
+                //pausa jogo
+                m_isPaused = true;
+
+                m_PauseScreen.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                //despausa jogo
+                m_isPaused = false;
+
+                m_PauseScreen.SetActive(false);
+                Time.timeScale = 1f;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Reinicia level atual
+    /// </summary>
+    public void RestartLevel ()
+    {
+        m_PauseScreen.SetActive(false);
+        LoadingController.Instance.ReloadLevel();
+    }
+
+    /// <summary>
+    /// Vai para o proximo level
+    /// </summary>
+    public void NextLevel (int indexLevel)
+    {
+        m_PauseScreen.SetActive(false);
+        LoadingController.Instance.NextLevel(indexLevel);
+    }
+}
